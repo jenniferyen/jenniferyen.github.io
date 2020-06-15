f4643601e1f3822dc308a494813e14a3619d6fb1
jennifer-yen <jennifer.yen@reddit.com>
2020-06-15 00:22:28 -0400

    Remove unnecessary files and start with base

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,11 @@
-import React from "react"
-// import { Link } from "gatsby"
+import React, { Component } from "react"
+import Base from "../components/base"
 
-import Layout from "../components/layout"
-// import Image from "../components/image"
-// import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
+const Index = () => (
+  <div>
     <h1>Jennifer Yen</h1>
-    {/* <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
-  </Layout>
+    <Base />
+  </div>
 )
 
-export default IndexPage
+export default Index
