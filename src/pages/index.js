fb3ae5b8229c2a0f530dd33b48bad63154daead5
Jennifer Yen <jennifer.yenjy@gmail.com>
2021-02-09 01:36:57 -0500

    Add Google Analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,12 @@ import React from "react"
 import Base from "../components/base"
 import Bio from "../components/bio"
 import PhotoGallery from "../components/photogallery"
+import ReactGA from 'react-ga';
+
 import "../styles/index.css"
+
+ReactGA.initialize('UA-176658398-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const Index = () => (
   <div className="main">
