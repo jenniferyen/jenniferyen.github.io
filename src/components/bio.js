cb835236d59b9b7e09ce42cd42f95c61067031b9
Jennifer Yen <jennifer.yenjy@gmail.com>
2021-02-08 22:25:08 -0500

    Reorganize CSS <a> and <span>

--- src/components/bio.js
+++ src/components/bio.js
@@ -13,27 +13,27 @@ function Bio(props) {
     return(
         <div className="bio">
             <h1>Hey, I'm <span style={{color: "#00ffff"}}>Jen</span>.</h1>
-            <h2>I'm currently tinkering with things at 
-                <span style={{color: "#00ffff"}}> UPenn </span> 
+            <h2>I'm currently tinkering with things at
+                <span style={{color: "#00ffff"}}> UPenn </span>
                 and am also developing for
-                <a href={neutral_url} target="_blank" rel="noopener noreferrer"> <span style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#00ffff"}}>Neutral.</span></a>
+                <span> <a href={neutral_url} target="_blank" rel="noopener noreferrer" style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#00ffff"}}>Neutral</a></span>.
             </h2>
-            <h2>I'll be 
-                <span style={{color: "#FFD271"}}> @Airbnb </span> 
-                soon and have learned tons from my previous experiences 
-                <span style={{color: "#FFD271"}}> @Reddit </span> 
-                and <a href={twitter_url} target="_blank" rel="noopener noreferrer"><span style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#FFD271"}}> @Twitter.</span></a>
+            <h2>I'll be
+                <span style={{color: "#FFD271"}}> @Airbnb </span>
+                soon and have learned tons from my previous experiences
+                <span style={{color: "#FFD271"}}> @Reddit </span>
+                and <span><a href={twitter_url} target="_blank" rel="noopener noreferrer" style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#FFD271"}}>@Twitter</a></span>.
             </h2>
-            <h2>I'm in some awesome VC communities like 
-                <a href={kp_url} target="_blank" rel="noopener noreferrer"> <span style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#FFD271"}}>Kleiner Perkins Fellows</span> </a>, 
-                <a href={ggv_url} target="_blank" rel="noopener noreferrer"> <span style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#FFD271"}}>GGV Capital NextGen Fellows</span> </a>, 
-                and <a href={wiress_url} target="_blank" rel="noopener noreferrer"><span style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#FFD271"}}> The Wiress.</span></a>
+            <h2>I'm in some awesome VC communities like
+                <span> <a href={kp_url} target="_blank" rel="noopener noreferrer" style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#FFD271"}}>Kleiner Perkins Fellows</a></span>, 
+                <span> <a href={ggv_url} target="_blank" rel="noopener noreferrer" style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#FFD271"}}>GGV Capital NextGen Fellows</a></span>, 
+                and <span><a href={wiress_url} target="_blank" rel="noopener noreferrer" style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#FFD271"}}>The Wiress</a></span>.
             </h2>
-            <h2>I like to 
-                <a href={strava_run_url} target="_blank" rel="noopener noreferrer"> <span style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#ff073a"}}>run</span></a>, 
-                <a href={strava_bike_url} target="_blank" rel="noopener noreferrer"> <span style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#ff073a"}}>bike</span></a>, 
+            <h2>I like to
+                <span> <a href={strava_run_url} target="_blank" rel="noopener noreferrer" style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#ff073a"}}>run</a></span>, 
+                <span> <a href={strava_bike_url} target="_blank" rel="noopener noreferrer" style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#ff073a"}}>bike</a></span>, 
                 <span style={{color: "#ff073a"}}> take photos (see below)</span>, 
-                and <a href={photoshop_url} target="_blank" rel="noopener noreferrer"><span style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#ff073a"}}>make my friends look epic</span></a>. 
+                and <span><a href={photoshop_url} target="_blank" rel="noopener noreferrer" style={{color: "#ffffff", textDecoration: "underline", textDecorationColor: "#ff073a"}}>make my friends look epic</a></span>. 
                 Talk to me about climate tech, history and culture, and blockchain (or anything really)!
             </h2>
             <h2>I'll update this site soon. I have so many thoughts :)</h2>
